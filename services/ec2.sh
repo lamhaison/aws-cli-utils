8ea18907f7617d03ba0acd3744242b8e27d9fc4f
Admin <lamhaison@gmail.com>
2022-12-26 13:11:12 +0700

    [update] - add validate for input of image_id and instance_id for aws_ec2 functions

--- services/ec2.sh
+++ services/ec2.sh
@@ -17,26 +17,28 @@ aws_ec2_reboot() {
 	aws ec2 reboot-instances --instance-ids $instance_id
 }
 
-
+# Ec2 image
 aws_ec2_list_images() {
 	aws ec2 describe-images --owners self
 }
 aws_ec2_create_image() {
 	instance_id=$1
+	echo To create a image from the ec2 instance ${instance_id:?"The instace_id is unset or empty"}
 	aws_ec2_instance_name=$(aws ec2 describe-instances \
 		--instance-ids $instance_id \
 		--query 'Reservations[*].Instances[*].{Tags:Tags[?Key == `Name`] | [0].Value}' \
 		--output text)
 
 	aws ec2 create-image \
-	    --no-reboot \
-	    --instance-id $instance_id \
-	    --name ${aws_ec2_instance_name}-`date '+%Y-%m-%d-%H-%M-%S'` \
-	    --description ${aws_ec2_instance_name}-`date '+%Y-%m-%d-%H-%M-%S'` \
-	    --query "ImageId" --output text
+		--no-reboot \
+		--instance-id $instance_id \
+		--name ${aws_ec2_instance_name}-$(date '+%Y-%m-%d-%H-%M-%S') \
+		--description ${aws_ec2_instance_name}-$(date '+%Y-%m-%d-%H-%M-%S') \
+		--query "ImageId" --output text
 }
 
 aws_ec2_get_image() {
 	image_id=$1
+	echo Get detail of the image ${image_id:?"The image_id is unset or empty"}
 	aws ec2 describe-images --image-ids $image_id
 }

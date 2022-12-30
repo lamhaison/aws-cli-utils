2314ce720e0ba14c14d4d9ee96c7f8b0ffb1e139
Admin <lamhaison@gmail.com>
2022-12-30 17:41:08 +0700

    [update] - to optimize check logs

--- main.sh
+++ main.sh
@@ -3,6 +3,7 @@ export tmp_credentials="/tmp/aws_temporary_credentials"
 export aws_cli_results="$(echo ~/aws_cli_results)"
 export aws_cli_history="${aws_cli_results}/history.json"
 export aws_assume_role_expired_time=55
+export aws_tmp_input="/tmp/aws_tmp_input_23647494949484.txt"
 
 # Get the current directory of the main.sh script.
 export AWS_CLI_SOURCE_SCRIPTS="$(dirname -- "$0")"
@@ -18,6 +19,13 @@ alias aws-cli-save-all-commandlines-to-history='history |grep aws | grep -v hist
 # alias assume-role='function(){eval $(command assume-role --duration 1h $@);}'
 
 # Import sub-commandline.
-for script in $(ls ${AWS_CLI_SOURCE_SCRIPTS}/services); do
-	source ${AWS_CLI_SOURCE_SCRIPTS}/services/$script
+
+for module in $(echo "common services")
+do
+	for script in $(ls ${AWS_CLI_SOURCE_SCRIPTS}/${module}); do
+		source ${AWS_CLI_SOURCE_SCRIPTS}/${module}/$script
+	done
 done
+
+
+
--- services/logs.sh
+++ services/logs.sh
@@ -12,11 +12,22 @@ aws_logs_tail() {
 
 aws_logs_tail_with_hint() {
         echo "List log groups"
-        aws_logs_list
+        common_input_to_tmp $(aws logs describe-log-groups --query "*[].logGroupName" --output text)
         echo "Your log group name >"
         read aws_log_group_name
 	aws_logs_tail $aws_log_group_name
 }
+
+
+aws_logs_tail_with_hint_peco() {
+        echo "List log groups"
+        aws_cw_log_group_names=$(aws logs describe-log-groups --query "*[].logGroupName" --output text)
+        echo "Your log group name >"
+        aws_log_group_name=$(echo ${aws_cw_log_group_names} | tr "\t" "\n" | peco)
+        echo You want to get log of ${aws_log_group_name:?"Your aws_log_group_name is unset or empty"}
+	aws_logs_tail $aws_log_group_name
+}
+
 
 
 

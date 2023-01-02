9011583414a0d79192e38baea6df9b401f052901
lamhaison <lamhaison@gmail.com>
2023-01-02 21:00:29 +0700

    [update] - to reduce number character the start and end for logs

--- services/common.sh
+++ services/common.sh
@@ -1,7 +1,7 @@
 aws_run_commandline() {
 	aws_commandline=$1
-	echo "---------------------------------------------------------START--$(date '+%Y-%m-%d-%H-%M-%S')-----------------------------------------------------------------" >> ${aws_cli_logs}/${ASSUME_ROLE}.log
+	echo "-------------------------------------START--$(date '+%Y-%m-%d-%H-%M-%S')------------------------------------------------" >> ${aws_cli_logs}/${ASSUME_ROLE}.log
 	echo Running commandline \[ ${aws_commandline:?"Commandline is unset or empty"}\ ] | tee -a ${aws_cli_logs}/${ASSUME_ROLE}.log
 	eval $aws_commandline | tee -a ${aws_cli_logs}/${ASSUME_ROLE}.log
-	echo "---------------------------------------------------------FINISH-$(date '+%Y-%m-%d-%H-%M-%S')-----------------------------------------------------------------" >> ${aws_cli_logs}/${ASSUME_ROLE}.log
+	echo "-------------------------------------FINISH-$(date '+%Y-%m-%d-%H-%M-%S')------------------------------------------------" >> ${aws_cli_logs}/${ASSUME_ROLE}.log
 }

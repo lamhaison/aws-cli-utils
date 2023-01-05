a2b944f329464536cf61092fbe538a5b22ae7cc2
Admin <lamhaison@gmail.com>
2023-01-05 10:52:48 +0700

    [update] - to remove function aws_logs_follow and set detail --follow for the tail function

--- services/logs.sh
+++ services/logs.sh
@@ -8,12 +8,6 @@ aws_logs_list() {
 aws_logs_tail() {
 	aws_log_group_name=$1
 	echo Get log of the group name ${aws_log_group_name:?"aws_log_group_name is unset or empty"}
-	aws logs tail $aws_log_group_name  --since ${aws_log_tail_since}
-}
-
-aws_logs_follow() {
-	aws_log_group_name=$1
-	echo Get log of the group name ${aws_log_group_name:?"aws_log_group_name is unset or empty"}
 	aws logs tail --follow $aws_log_group_name  --since ${aws_log_tail_since}
 }
 
@@ -22,13 +16,3 @@ aws_logs_tail_with_hint() {
         aws_log_group_name=$(echo "$(peco_aws_logs_list)" | peco)
 	aws_logs_tail $aws_log_group_name
 }
-
-aws_logs_follow_with_hint() {
-        echo "Your log group name >"
-        aws_log_group_name=$(echo "$(peco_aws_logs_list)" | peco)
-	aws_logs_follow $aws_log_group_name
-}
-
-
-
-

1ca0b4ef43d2524b5f39f78ff4e6e2d1e3c6c5b3
Admin <lamhaison@gmail.com>
2023-01-16 15:55:14 +0700

    [update] - add comment field when list cloudfront - easy to read instead of only show id

--- services/cloudfront.sh
+++ services/cloudfront.sh
@@ -5,19 +5,23 @@ aws_cloudfront_list_detail() {
 }
 
 aws_cloudfront_list() {
-	aws_run_commandline 'aws cloudfront list-distributions --query "DistributionList.Items[*].{Id:Id,Aliases:Aliases}"'
+	aws_run_commandline \
+		"
+			aws cloudfront list-distributions \
+				--query 'DistributionList.Items[*].{Id:Id,Aliases:Aliases,Comment:Comment}'
+		"
 
 }
 
 aws_cloudfront_get() {
-	distribution_id=$1
-	echo Get information for cloudfront ${distribution_id:?"Distribution id is uset or empty"}
-	aws_run_commandline "aws cloudfront get-distribution --id ${distribution_id}"
+	aws_distribution_id=$1
+	echo Get information for cloudfront ${aws_distribution_id:?"Distribution id is uset or empty"}
+	aws_run_commandline "aws cloudfront get-distribution --id ${aws_distribution_id}"
 }
 
 aws_cloudfront_list_with_hint() {
 	aws_cloudfront_list
 	echo "Your Distribution ID >"
-	read distribution_id
-	aws_cloudfront_get ${distribution_id}
+	read aws_distribution_id
+	aws_cloudfront_get ${aws_distribution_id}
 }

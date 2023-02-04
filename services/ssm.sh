55d22edc5381a46d0b0d09e07192b7e6277966f8
lamhaison <lamhaison@gmail.com>
2023-02-05 00:08:05 +0700

    [update] - add feature to connect ssm to ec2 with hint commandline

--- services/ssm.sh
+++ services/ssm.sh
@@ -13,8 +13,8 @@ aws_ssm_list_parameters() {
 
 aws_ssm_connection_ec2() {
         aws_ec2_instance_id=$1
-        echo 'Connect to the ec2 instance ${aws_ec2_instance_id:?"aws_ec2_instance_id is unset or empty"} \
-                by commandline [ aws ssm start-session --target $aws_ec2_instance_id ]'
+        echo "Connect to the ec2 instance ${aws_ec2_instance_id:?'aws_ec2_instance_id is unset or empty'} \
+                by commandline [ aws ssm start-session --target $aws_ec2_instance_id ]"
 
         if [[ -n "${aws_ec2_instance_id}" ]]; then
                 aws ssm start-session --target $aws_ec2_instance_id

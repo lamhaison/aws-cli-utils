543c170352878dd1707437f0cd2a0ef2d32d68a2
Admin <lamhaison@gmail.com>
2023-02-13 15:36:11 +0700

    [add] - add function aws_rds_share_db_cluster_snapshot_with_other_aws_account

--- services/rds.sh
+++ services/rds.sh
@@ -157,6 +157,14 @@ aws_rds_create_db_instance_snapshot() {
     "
 }
 
+aws_rds_share_db_cluster_snapshot_with_other_aws_account() {
+	aws_rds_db_cluster_name=$1
+	aws_shared_account_id=$2
+	aws rds modify-db-cluster-snapshot-attribute \
+		--db-cluster-snapshot-identifier ${aws_rds_db_cluster_name} \
+		--attribute-name restore --values-to-add ${aws_shared_account_id}
+}
+
 aws_rds_delete_db_snapshot() {
 	aws_rds_db_snapshot_name=$1
 	aws_run_commandline "\

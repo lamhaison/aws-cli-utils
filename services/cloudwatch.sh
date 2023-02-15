0402362d3306aa1b408fce75d72cf4aa1b55d0c5
Admin <lamhaison@gmail.com>
2023-02-15 17:17:56 +0700

    [add] - add function to get aws_cloudwatch_get_graph

--- services/cloudwatch.sh
+++ services/cloudwatch.sh
@@ -11,3 +11,15 @@ aws_cloudwatch_list_alb_arn() {
 		aws elbv2 describe-load-balancers --query '*[].LoadBalancerArn'
 	"
 }
+
+aws_cloudwatch_get_graph() {
+	local aws_cloudwatch_widget_image=$1
+	local graph_file_file_name="$(lamhaison_file_name_get_random_name ${ASSUME_ROLE}).png"
+	local graph_file_path="${aws_cli_images}/${ASSUME_ROLE}"
+	mkdir -p ${graph_file_path}
+	aws cloudwatch get-metric-widget-image \
+		--metric-widget ${aws_cloudwatch_widget_image} \
+		--output-format png --output text | base64 --decode >${graph_file_path}/${graph_file_file_name}
+
+	echo "Access the graph by the url ${graph_file_path}/${graph_file_file_name}"
+}

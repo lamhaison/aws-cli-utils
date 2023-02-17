e682265672e3db7089ab2e418eda0c1b7bae419d
Admin <lamhaison@gmail.com>
2023-02-17 10:42:04 +0700

    [Refactor] - add default value for aws_sg_id when it is empty or unset and add suffic instruction for functions

--- services/ec2.sh
+++ services/ec2.sh
@@ -130,15 +130,15 @@ aws_sg_get() {
 	"
 }
 
-aws_sg_add_rule() {
+aws_sg_add_rule_instruction() {
 	aws_sg_id=$1
 
 	echo "\
 		# Allow access the ssh from a specific IP address
 		aws ec2 authorize-security-group-ingress \
-		--group-id ${aws_sg_id:?'aws_sg_id is unset or empty'} \
+		--group-id ${aws_sg_id:="\$aws_sg_id"} \
 		--protocol tcp --port 22 \
-		--cidr $(lamhaison_get_public_ip)/32
+		--cidr $(dig +short myip.opendns.com @resolver1.opendns.com)/32
 	"
 }
 

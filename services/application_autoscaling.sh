a10efbf684d697f33a5fdb18ede9a839cfc7189d
Admin <lamhaison@gmail.com>
2023-02-28 17:51:52 +0700

    [Add] - to add function to list ecs scaling policies

--- services/application_autoscaling.sh
+++ services/application_autoscaling.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Setting for ecs
-aws_application_autoscaling_ecs() {
+aws_application_autoscaling_ecs_list_scheduled_actions() {
 	aws application-autoscaling describe-scheduled-actions \
 		--service-namespace ecs
 }

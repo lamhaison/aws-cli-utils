fe2744de642199355a9a6a010083ac6bcdacfa1a
Admin <lamhaison@gmail.com>
2023-03-16 17:15:32 +0700

    [Add] - add functions for get stack set and list all stack set with detail

--- services/cloudformation.sh
+++ services/cloudformation.sh
@@ -4,3 +4,19 @@
 aws_cloudformation_list_stack_sets() {
 	aws_run_commandline "aws cloudformation list-stack-sets"
 }
+
+aws_cloudformation_get_stack_set() {
+	aws_run_commandline "\
+		aws cloudformation describe-stack-set \
+			--stack-set-name ${1:?'stack_set_name is unset or empty'}
+
+	"
+}
+
+aws_cloudformation_stack_info() {
+
+	local lhs_stack_set_names=$(aws cloudformation list-stack-sets --query '*[].StackSetName' --output text)
+	for stack_set_name in ${lhs_stack_set_names}; do
+		aws_cloudformation_get_stack_set ${stack_set_name}
+	done
+}

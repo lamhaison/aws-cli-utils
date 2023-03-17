3cadc371ca9a3f2195a8bd160182e2192f540056
hungnt1412 <hungnt1214@gmail.com>
2023-03-17 13:16:43 +0700

    [update] ssm - get value parameter store with hint

--- services/ssm.sh
+++ services/ssm.sh
@@ -11,6 +11,11 @@ aws_ssm_list_parameters() {
         "
 }
 
+aws_ssm_get_value_parameter_with_hint() {
+  parameter_name=$(peco_create_menu 'peco_aws_ssm_list_parameters' '--prompt "Choose Parameter Name >"')
+  aws_run_commandline "aws ssm get-parameter --name $parameter_name --with-decryption --query 'Parameter.Value' --output text"
+}
+
 aws_ssm_connection_ec2() {
         aws_ec2_instance_id=$1
         aws_commandline_logging "\

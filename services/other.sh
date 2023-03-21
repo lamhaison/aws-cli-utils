45dce233031da05e0b74210096e20498cd82fae0
Admin <lamhaison@gmail.com>
2023-03-21 15:07:29 +0700

    [Add] - add function aws_resource_list to list all resources on aws

--- services/other.sh
+++ services/other.sh
@@ -13,3 +13,22 @@ aws_datapipeline_check_using() {
 aws_autoscaling_lauching_configuration_list() {
 	aws autoscaling describe-launch-configurations --query "*[].LaunchConfigurationName"
 }
+
+aws_resource_list() {
+
+	aws_run_commandline "\
+		aws resourcegroupstaggingapi get-resources
+	"
+}
+
+#
+# TODO aws_resource_list_by_tag_name to list all resource that have tag Name is
+# @param	TODO The first parameter is the tag_name value
+# @return List resources that match the tag name which you pass to the function.
+#
+aws_resource_list_by_tag_name() {
+	aws_run_commandline "\
+		aws resourcegroupstaggingapi get-resources \
+			--tag-filters 'Key=Name,Values=${1:?lhs_aws_resource_tag_name is unset or empty}'
+	"
+}

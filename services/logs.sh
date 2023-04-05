49d99c3c5a7898aeaa18f0eeb3c88d1e520112f5
lamhaison <lamhaison@gmail.com>
2023-04-06 05:30:15 +0700

    [Improvement] - add option to set get from from x time instead of 120m as the default

--- services/logs.sh
+++ services/logs.sh
@@ -7,14 +7,16 @@ aws_logs_list() {
 
 aws_logs_tail() {
 	aws_log_group_name=$1
+	local aws_logs_sinces=${2:-$aws_log_tail_since}
 	echo Get log of the group name ${aws_log_group_name:?"aws_log_group_name is unset or empty"}
-	echo "Running commandline [ aws logs tail --follow $aws_log_group_name --since ${aws_log_tail_since} ]"
-	aws logs tail --follow $aws_log_group_name --since ${aws_log_tail_since}
+	local aws_cmd="aws logs tail --follow $aws_log_group_name --since ${aws_logs_sinces}"
+	lhs_commandline_logging "${aws_cmd}"
+	eval ${aws_cmd}
 }
 
 aws_logs_tail_with_hint() {
 	echo "Your log group name >"
 	aws_log_group_name=$(peco_create_menu 'peco_aws_logs_list')
 
-	aws_logs_tail $aws_log_group_name
+	aws_logs_tail $aws_log_group_name $1
 }

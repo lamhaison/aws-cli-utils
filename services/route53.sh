270dee0f9971f8ec0e0fc66d1ccb4b793863a253
lamhaison <lamhaison@gmail.com>
2023-08-03 11:12:36 +0700

    [Add] - aws route 53 get hotzone with hint

--- services/route53.sh
+++ services/route53.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 # AWS Route53
-aws_route53_list() {
-	# aws_run_commandline 'aws route53 list-hosted-zones --query "*[].Name"'
-	aws_run_commandline 'aws route53 list-hosted-zones --query \
-	"*[].{Id:Id,Name:Name,PrivateZone:Config.PrivateZone}" --output table'
+
+function peco_aws_route53_list() {
+	commandline="aws route53 list-hosted-zones \
+		--query '*[].{AId:Id,Name:Name}' --output text | tr -s '\t' '_'"
+	peco_commandline_input ${commandline} 'true'
 }
 
-aws_route53_get() {
-	aws_route53_get_host_zone $1
+function aws_route53_list() {
+	aws_run_commandline "\
+		aws route53 list-hosted-zones --query \
+			'*[].{Id:Id,Name:Name,PrivateZone:Config.PrivateZone}' --output table
+	"
 }
-aws_route53_get_host_zone() {
+
+function aws_route53_get_host_zone() {
 	aws_route53_host_zone_id=$1
 
 	aws_run_commandline "\
@@ -22,4 +27,15 @@ aws_route53_get_host_zone() {
 				ResourceRecords:ResourceRecords[0].Value}' \
 			--output table
 	"
+}
+
+function aws_route53_get() {
+	aws_route53_get_host_zone $1
+}
+
+function aws_route53_get_with_hint() {
+
+	local lhs_input=$(peco_create_menu 'peco_aws_route53_list' '--prompt "Choose the domain >"')
+	local aws_route53_host_zone_id=$(echo ${lhs_input} | awk -F "_" '{print $1}')
+	aws_route53_get $aws_route53_host_zone_id
 }

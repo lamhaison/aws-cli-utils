5b24f24ef88b3edee3f4ec60b986dcfda1db94dc
lamhaison <lamhaison@gmail.com>
2024-07-01 10:05:58 +0700

    [Update] - Route53 functions
    List function - more information when list hostzones
    Refactor function name get info hostzone from aws_route53_get to aws_route53_get_host_zone
    Add function get hostzone with hint (you can select which hostzone to view)
    Add function to get list records in hostzone

--- services/route53.sh
+++ services/route53.sh
@@ -10,11 +10,23 @@ function peco_aws_route53_list() {
 function aws_route53_list() {
 	aws_run_commandline "\
 		aws route53 list-hosted-zones --query \
-			'*[].{Id:Id,Name:Name,PrivateZone:Config.PrivateZone}' --output table
+			'*[].{Id:Id,Name:Name,PrivateZone:Config.PrivateZone,Comment:Config.Comment,RecordSetCoun:ResourceRecordSetCount}' --output table
 	"
 }
 
-function aws_route53_get_host_zone() {
+function aws_route53_get() {
+	aws_route53_host_zone_id=$1
+
+	# Check input invalid
+	if [[ -z "$aws_route53_host_zone_id" ]]; then return; fi
+
+	aws_run_commandline "\
+		aws route53 get-hosted-zone --id ${aws_route53_host_zone_id}
+	"
+
+}
+
+function aws_route53_get_records() {
 	aws_route53_host_zone_id=$1
 
 	aws_run_commandline "\
@@ -29,22 +41,21 @@ function aws_route53_get_host_zone() {
 	"
 }
 
-function aws_route53_get() {
-	aws_route53_get_host_zone $1
-}
-
 function aws_route53_get_with_hint() {
 
+	# shellcheck disable=SC2155
 	local lhs_input=$(peco_create_menu 'peco_aws_route53_list' '--prompt "Choose the domain >"')
+	# shellcheck disable=SC2155
 	local aws_route53_host_zone_id=$(echo ${lhs_input} | awk -F "_" '{print $1}')
-	aws_route53_get $aws_route53_host_zone_id
+
+	aws_route53_get "${aws_route53_host_zone_id}"
 }
 
-function aws_route_get_host_zone_with_hint() {
+function aws_route53_get_records_with_hint() {
 
+	# shellcheck disable=SC2155
 	local lhs_input=$(peco_create_menu 'peco_aws_route53_list' '--prompt "Choose the domain >"')
+	# shellcheck disable=SC2155
 	local aws_route53_host_zone_id=$(echo ${lhs_input} | awk -F "_" '{print $1}')
-
-	aws route53 get-hosted-zone --id ${aws_route53_host_zone_id}
-
+	aws_route53_get_records "$aws_route53_host_zone_id"
 }

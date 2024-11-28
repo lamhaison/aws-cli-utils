1dbf9f0d1bc894ce680018dd5bb89bf1a63b6785
lamhaison <lamhaison@gmail.com>
2024-11-28 15:25:22 +0700

    [Update] - fix get account id before generate the docker image url on ecr

--- services/ecr.sh
+++ services/ecr.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-aws_ecr_list_repos() {
+function aws_ecr_list_repos() {
 	aws_run_commandline "aws ecr describe-repositories --query \"*[].repositoryArn\""
 }
 
-aws_ecr_login() {
+function aws_ecr_login() {
 
 	if [[ -z "${AWS_ACCOUNT_ID}" ]]; then
 		aws_assume_role_get_aws_account_id
@@ -21,7 +21,7 @@ aws_ecr_login() {
 
 }
 
-aws_ecr_logout() {
+function aws_ecr_logout() {
 	if [[ -z "${AWS_ACCOUNT_ID}" ]]; then
 		aws_assume_role_get_aws_account_id
 	fi
@@ -29,7 +29,7 @@ aws_ecr_logout() {
 
 }
 
-aws_ecr_list_images() {
+function aws_ecr_list_images() {
 	if [[ -z "${AWS_ACCOUNT_ID}" ]]; then
 		aws_assume_role_get_aws_account_id
 	fi
@@ -43,7 +43,7 @@ aws_ecr_list_images() {
 	aws_run_commandline "aws ecr list-images --repository-name ${aws_ecr_repo_name} --query 'imageIds[].{imageTag:imageTag}'"
 }
 
-aws_ecr_get_latest_image() {
+function aws_ecr_get_latest_image() {
 	if [[ -z "${AWS_ACCOUNT_ID}" ]]; then
 		aws_assume_role_get_aws_account_id
 	fi
@@ -57,17 +57,17 @@ aws_ecr_get_latest_image() {
 	aws_run_commandline "aws ecr list-images --repository-name ${aws_ecr_repo_name} --query 'imageIds[0].{imageTag:imageTag}'"
 }
 
-aws_ecr_get_latest_image_with_hint() {
+function aws_ecr_get_latest_image_with_hint() {
 	echo "Your repository name >"
 	aws_ecr_get_latest_image $(peco_create_menu 'peco_aws_ecr_list_repo_names')
 }
 
-aws_ecr_list_images_with_hint() {
+function aws_ecr_list_images_with_hint() {
 	echo "Your repository name >"
 	aws_ecr_list_images $(peco_create_menu 'peco_aws_ecr_list_repo_names')
 }
 
-aws_ecr_get_image() {
+function aws_ecr_get_image() {
 	aws_ecr_repo_name=$1
 	aws_ecr_repo_image_tag=$2
 
@@ -80,7 +80,8 @@ aws_ecr_get_image() {
 		${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${aws_ecr_repo_name:?Repo is not set or empty}:${aws_ecr_repo_image_tag:-latest}
 }
 
-aws_ecr_pull_image_with_hint() {
+function aws_ecr_pull_image_with_hint() { # To get the docker image and pull it to local
+	aws_assume_role_get_aws_account_id
 	echo "Your repository name >"
 	aws_ecr_repo_name=$(peco_create_menu 'peco_aws_ecr_list_repo_names')
 	echo "Your image tag for ${aws_ecr_repo_name:?'aws_ecr_repo_name is unset or empty'} >"
